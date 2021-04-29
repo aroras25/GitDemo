6ad26ff14a42bad84dcf6f47cbc529cb008674fd
sahil <arora.sahil4832@gmail.com>
2021-04-30 00:02:59 +0530

    My First Commit

---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void AppiumTest_1() {
+		
+		System.out.println("AppiumTest_1");
+	}
+}
---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void SeleniumTest_1() {
+		
+		System.out.println("SeleniumTest_1");
+	}
+	
+	@Test
+	public void SeleniumTest_2() {
+		
+		System.out.println("SeleniumTest_2");
+	}
+
+}
